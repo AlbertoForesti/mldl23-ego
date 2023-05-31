c5ae2f274c10b63aba5d7d978e46ae1ed9a9a115
AlbertoForesti <alberto.foresti.01@icloud.com>
2023-05-31 23:50:37 +0200

    debug

--- models/OurTA3N.py
+++ models/OurTA3N.py
@@ -281,7 +281,7 @@ class BaselineTA3N(nn.Module):
             for new_order, original_order in enumerate(permutation): # iterates 5 times (number of clips in video)
                 correct_pred_indices = self.get_correct_pred_indices(original_order, new_order)
                 probs_weird = torch.sum(probs[:,correct_pred_indices], dim=1).unsqueeze(0)
-                raise UserWarning(f'weights={weights.shape}, probs={probs.shape}, probs_weird = {probs_weird.shape}')
+                # raise UserWarning(f'weights={weights.shape}, probs={probs.shape}, probs_weird = {probs_weird.shape}')
                 weights = torch.cat((weights, probs_weird))
             return weights
         
