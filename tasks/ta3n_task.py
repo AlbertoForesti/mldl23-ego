6ca0268b9cf24b9fec2aae844ef580e06c5499b4
AlbertoForesti <alberto.foresti.01@icloud.com>
2023-06-10 12:48:17 +0200

    debug

--- tasks/ta3n_task.py
+++ tasks/ta3n_task.py
@@ -126,10 +126,6 @@ class ActionRecognition(tasks.Task, ABC):
             cop_loss = self.criterion(pred_cop_all, label_cop_all)
             self.cop_loss.update(torch.mean(cop_loss) / (self.total_batch / self.batch_size), self.batch_size)
 
-            if self.model_args['RGB'].attention_cop:
-                for i in range(5):
-                    self.attn_cop_weights[i].update(features['attn_weights_cop'][i])
-
         
 
         if 'Gtd' in self.model_args['RGB'].blocks:
@@ -214,11 +210,6 @@ class ActionRecognition(tasks.Task, ABC):
             'top1-accuracy': self.accuracy.avg[1],
             'top5-accuracy': self.accuracy.avg[5]
         }
-        
-        if self.model_args['RGB'].attention_cop:
-            
-            for i in range(5):
-                logs[f'attn_cop_{i}']=self.attn_cop_weights[i].val
 
         # Log the learning rate, separately for each modality.
         for m in self.modalities:
