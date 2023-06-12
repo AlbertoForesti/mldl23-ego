61b7eb6a76343560e619548c9134f80e365c7db2
Giulia Bonino <60091519+GiuliaBonino@users.noreply.github.com>
2023-06-12 14:19:57 +0200

    debug

--- models/OurTA3N.py
+++ models/OurTA3N.py
@@ -50,7 +50,7 @@ class BaselineTA3N(nn.Module):
                 return
 
         if 'copnet' in self.model_config.cop_type:
-            out_features_dim_copnet = 2 if 'simple' in self.model_config.cop_type else factorial(self.model_config.cop_samples)
+            out_features_dim_copnet = 2 if 'simple' in self.model_config.cop_type else factorial(self.model_config.cop_samples, exact=True)
             self.permute_type = 'simple' if 'simple' in self.model_config.cop_type else 'complex'
             self.end_points['copnet'] = self.COPNet(in_features_dim, out_features_dim_copnet)
         
